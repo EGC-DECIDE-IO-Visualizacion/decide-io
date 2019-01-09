ac5a69ad9c870600f7e4693d68f85c1803ca5d2e
LordVanHellSing <angelfx19@gmail.com>
2019-01-09 22:13:04 +0100

    FEAT. Recuento automatico de Votos.
    Se ha tenido que implementar un metodo de ejemplo que simule actualizar el recuento de votos de una votacion.  Dicho metodo lo tenian que haber implementado el Equipo de Votacion pero no lo han podido realizar.

--- decide/visualizer/views.py
+++ decide/visualizer/views.py
@@ -3,6 +3,15 @@ from django.conf import settings
 from django.http import Http404
 
 from base import mods
+from datetime import datetime
+
+# Se ha tenido que implementar un metodo de ejemplo, el equipo del modulo Voting, no ha podido realizar el metodo "tally", que actualiza el recuento de los votos
+def updateVotingExample(voting_id):
+    text = "La votacion con el ID: %d se ha actualizado el {:%B %d, %Y}".format(datetime.now()) %voting_id
+
+    return text
+    
+    
 
 
 class VisualizerView(TemplateView):
@@ -14,7 +23,10 @@ class VisualizerView(TemplateView):
 
         try:
             r = mods.get('voting', params={'id': vid})
+            update = updateVotingExample(vid)
+            
             context['voting'] = r[0]
+            context['update'] = update
         except:
             raise Http404
 
